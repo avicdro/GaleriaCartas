43060d27e8100e39ce6431277af79ccaf7a9939d
avicdro <avicdro@gmail.com>
2017-08-24 16:52:09 -0430

    Varias animaciones1
    Animaciones: en el nav y en la carta
    Mucho css.

--- src/app/cards/cards.component.ts
+++ src/app/cards/cards.component.ts
@@ -1,14 +1,86 @@
 import { Component, OnInit } from '@angular/core';
+import {
+  trigger,
+  state,
+  style,
+  animate,
+  transition
+} from '@angular/animations';
 
 @Component({
   selector: 'mvat-cards',
   templateUrl: './cards.component.html',
-  styleUrls: ['./cards.component.css']
+  styleUrls: ['./cards.component.css'],
+  animations: [
+    trigger('opacar', [
+      state('inactivo', style({
+        opacity: '0',
+      })),
+      state('activo', style({
+        opacity: '1',
+      })),
+      transition('inactivo => activo', animate('600ms ease-in')),
+      transition('activo => inactivo', animate('600ms ease-out')),
+    ]),
+    trigger('transladaXT', [
+      state('inactivo', style({
+        transform: 'scale(0.7)',
+        top: '148px',
+        left: '-12px',
+      })),
+      state('activo', style({
+        transform: 'scale(1)',
+        top: '12px',
+        left: '12px'
+      })),
+      transition('inactivo => activo', animate('600ms ease-in')),
+      transition('activo => inactivo', animate('600ms ease-out')),
+    ]),
+    trigger('transladaXC', [
+      state('inactivo', style({
+        height: '25%',
+        top: '74%',
+      })),
+      state('activo', style({
+        height: '100%',
+        top: '0%',
+      })),
+      transition('inactivo => activo', animate('600ms ease-in')),
+      transition('activo => inactivo', animate('600ms ease-out')),
+    ])
+  ],
 })
 export class CardsComponent implements OnInit {
+  opacar: string;
+  transladaXT: string;
+  transladaXC: string;
+  constructor() {
+    this.transladaXT = 'activo';
+    this.transladaXC = 'activo';
+    this.opacar = 'activo';
+   }
 
-  constructor() { }
-
+   transladaXc(): void {
+    if (this.transladaXC === 'activo') {
+      this.transladaXC = 'inactivo';
+    } else {
+      this.transladaXC = 'activo'
+    }
+  }
+   transladaX(): void {
+    if (this.transladaXT === 'activo') {
+      this.transladaXT = 'inactivo';
+    } else {
+      this.transladaXT = 'activo'
+    }
+  }
+  opaca(): void {
+    if (this.opacar === 'activo') {
+      this.opacar = 'inactivo';
+    } else {
+      this.opacar = 'activo'
+    }
+  }
   ngOnInit() {
   }
 
--- src/app/nav-g/nav-g.component.ts
+++ src/app/nav-g/nav-g.component.ts
@@ -1,15 +1,40 @@
 import { Component, OnInit } from '@angular/core';
+import {
+  trigger,
+  state,
+  style,
+  animate,
+  transition
+} from '@angular/animations';
 
 @Component({
   selector: 'mvat-nav-g',
   templateUrl: './nav-g.component.html',
-  styleUrls: ['./nav-g.component.css']
+  styleUrls: ['./nav-g.component.css'],
+  animations: [
+    trigger('animacion', [
+      state('inactivo', style({
+        backgroundColor: '#03a0ff',
+        width: '0',
+        opacity: '0'
+      })),
+      state('activo', style({
+        width: '*',
+      })),
+      transition('inactivo => activo', animate('100ms ease-in')),
+      transition('activo => inactivo', animate('100ms ease-in')),
+    ])
+  ],
 })
 export class NavGComponent implements OnInit {
+  estado = 'inactivo';
 
-  constructor() { }
+  constructor() {
 
+  }
   ngOnInit() {
   }
+
+
 
 }
